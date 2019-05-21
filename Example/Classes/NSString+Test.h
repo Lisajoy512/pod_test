19cc504cbce56e145ff20fd9362905dff1829505
wuyine <769918579@qq.com>
2019-05-21 16:03:02 +0800

    1

---
+++ Example/Classes/NSString+Test.h
@@ -0,0 +1,17 @@
+//
+//  NSString+Test.h
+//  pod_test_Example
+//
+//  Created by wuyine on 2019/5/21.
+//  Copyright © 2019年 769918579@qq.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (Test)
+- (void)test;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/Classes/NSString+Test.m
@@ -0,0 +1,15 @@
+//
+//  NSString+Test.m
+//  pod_test_Example
+//
+//  Created by wuyine on 2019/5/21.
+//  Copyright © 2019年 769918579@qq.com. All rights reserved.
+//
+
+#import "NSString+Test.h"
+
+@implementation NSString (Test)
+- (void)test{
+    NSLog(@"只是一个测试");
+}
+@end
